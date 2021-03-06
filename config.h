2cd380c91761fbfcdbf752cedbb036c48410b647
Adam Podkowinski <adampodkdev@gmail.com>
2021-03-06 17:22:07 +0100

    some changes

--- config.h
+++ config.h
@@ -6,7 +6,9 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "FiraCode Nerd Font:pixelsize=15:antialias=true:autohint=true";
-static char *font2[] = { "Noto Color Emoji:pixelsize=10:antialias=true:autohint=true" };
+static char *font2[] = {"PowerlineSymbols:pixelsize=15:antialias=true",
+      		 	"Noto Color Emoji:pixelsize=10:antialias=true:autohint=true" };
+
 static int borderpx = 3;
 
 /*
@@ -107,7 +109,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 0.79;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
