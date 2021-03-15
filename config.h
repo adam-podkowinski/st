3d96010baafac5b69977728a972dbf844a6c0098
Adam Podkowinski <adampodkdev@gmail.com>
2021-03-15 17:01:52 +0100

    Change linespacing

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "FiraCode Nerd Font:pixelsize=14.5:antialias=true:autohint=true";
+static char *font = "JetBrainsMono Nerd Font:pixelsize=14.5:antialias=true:autohint=true";
 static char *font2[] = {"Hack Nerd Font:style=Medium:pixelsize=14.5",
       		 	"Noto Color Emoji:pixelsize=10:antialias=true:autohint=true" };
 
@@ -30,7 +30,7 @@ char *vtiden = "\033[?6c";
 
 /* Kerning / character bounding-box multipliers */
 static float cwscale = 1.0;
-static float chscale = 1.0;
+static float chscale = 1.1;
 
 /*
  * word delimiter string
