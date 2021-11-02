89836321b155e672ea664dfbe915e9573cac096a
adampodkdev@gmail.com <adampodkdev@gmail.com>
2021-11-02 22:22:48 +0100

    change line height and font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "FiraCode Nerd Font:pixelsize=14:antialias=true:autohint=true";
+static char *font = "FiraCode Nerd Font:style=Retina:pixelsize=14:antialias=true:autohint=true";
 static char *font2[] = {
   "Hack:style=Regular:pixelsize=14:antialias=true:autohint=true",
   "DejaVu Sans Mono for Powerline:pixelsize=14:antialias=true:autohint=true",
@@ -33,7 +33,7 @@ char *vtiden = "\033[?6c";
 
 /* Kerning / character bounding-box multipliers */
 static float cwscale = 1.0;
-static float chscale = 1.1;
+static float chscale = 1.3;
 
 /*
  * word delimiter string
